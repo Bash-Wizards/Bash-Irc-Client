546b119f651e19ca37f54a4c27f298648ab414f0
TripleZer0 <57972505+TripleZer000@users.noreply.github.com>
2021-04-25 02:22:59 -0500

    v0.3
    fixed STATUS: line

--- bot.sh
+++ bot.sh
@@ -6,22 +6,24 @@ term_height=0
 term_width=0
 term_scroll_height=0
 status_line_row=0
-irc_host=''
-irc_channel=''
-irc_nick=''
 
+function paste_data() {
+	echo -en '\e7' "\e[${term_scroll_height};0f" '\n' >&2
+	echo -n " $1" >&2
+	echo -en '\e8' >&2
+}
 
 function scroll_bottom() {
-	echo -en '\e[999B'
+	echo -en '\e[999B' >&2
 }
 
 #figure out terminal height, NOTE: moves cursor to bottom of terminal
 function term_height() {
-	echo -en '\e[999B\e[6n'
-	read -s -r -d'['
+	echo -en '\e[999B\e[6n' >&2
+	read -s -r -d'[' >&2
 	read -s -r -d';' term_height
-	read -s -r -d'R' term_width
-	echo -en '\e[999D'
+	read -s -r -d'R' term_width >&2
+	echo -en '\e[999D' >&2
 }
 
 #set the area the terminal is allowed to scroll in
@@ -35,51 +37,61 @@ function bottom_line() {
 	echo -en "\e[${term_height};0f"
 }
 
-function paste_data() {
-	echo -en '\e7' "\e[${term_scroll_height};0f" '\n'
-	echo -n " $1"
-	echo -en '\e8'
-}
-
 function status_line() {
-	echo -en '\e7' "\e[${status_line_row};0f" '\e[2K'
-	echo -en "\e[4;44mSTATUS: $irc_nick in $irc_channel @ $irc_host\e[0m"
-	echo -en '\e8'
+		echo -en '\e7' "\e[${status_line_row};0f" '\e[2K' >&2
+		echo -en "\e[4;44mSTATUS: $nick in $channel @ $server\e[0m" >&2
+		echo -en '\e8' >&2
 }
 
 function init_screen() {
-	echo -en '\e[r' #reset screen RE-ADD MEEE FOR A HOTFIX
+	echo -en '\e[r' >&2 #reset screen helps for clearing your terminal upon boot
 	term_height
 	scroll_helper
 	bottom_line
 }
 
 function read_line() {
+local buff=''
 while true
 do
-	while IFS='' read -r -t1
+## Below is the read Line!!!
+	while read -r -t1 REPLY
 	do 
-		status_line
-		echo -en '\e[2K\r> '
-		case "$REPLY" in
-		( :reset )
-			init_screen
-		break
-		;;
-		( * )
-			echo "$REPLY" >> $input	
-		;;
-		esac	
+		echo -en '\e7' "\e[${status_line_row};0f" '\e[2K' >&2
+		echo -en "\e[4;44mSTATUS: $nick @ $server\e[0m" >&2
+		echo -en '\e8' >&2
+		echo -en '\e[2K\rINPUT> ' >&2
+## By deafault below is enabled
+## Below is for single channel use where INPUT is as simple as typing "Hello World" and it will echo it to IRC
+## Denote below lines and continue to next notes for multichannel config 
+		echo "PRIVMSG $channel :$REPLY" >> $input	
+			buff="<$nick> $channel :${REPLY}"
+			paste_data "$buff"
+## Below is the multichannel config (CURRENTLY UNDER DEVELOPMENT
+#		case "$REPLY" in
+#		( /JOIN* )
+#			channel="${REPLY##*\ }"
+#			echo "$REPLY" >> $input
+#			;;
+#		( * )
+#			echo "PRIVMSG $channel :$REPLY" >> $input
+#			;;
+#		esac
+#			buff="<$nick> ${REPLY}"
+#			paste_data "$buff"
 	done
 done
 }
 
+
 . bot.properties
 input=".bot.cfg"
+local twig=''
 echo "Starting session: $(date "+[%y:%m:%d %T]")">$log 
 echo "NICK $nick" > $input 
 echo "USER $user" >> $input
-#echo "JOIN #$channel" >> $input
+echo "JOIN $channel" >> $input
+
 scroll_bottom
 init_screen
 
@@ -104,33 +116,15 @@ do
 	 echo "PRIVMSG $channel :this is the output" >> $input
 	;;
 ################### ^^^^^^^^^^^^^^
+## ADD QUIT AND JOIN MSGS BELOW
     # run when a message is seen
     *PRIVMSG*)
-      echo "$res"
-      who=$(echo "$res" | perl -pe "s/:(.*)\!.*@.*/\1/")
-      from=$(echo "$res" | perl -pe "s/.*PRIVMSG (.*[#]?([a-zA-Z]|\-)*) :.*/\1/")
-      # "#" would mean it's a channel
-      if [ "$(echo "$from" | grep '#')" ]
-      then
-        test "$(echo "$res" | grep ":$nick:")" || continue
-        will=$(echo "$res" | perl -pe "s/.*:$nick:(.*)/\1/")
-      else
-        will=$(echo "$res" | perl -pe "s/.*$nick :(.*)/\1/")
-        from=$who
-      fi
-      will=$(echo "$will" | perl -pe "s/^ //")
-      com=$(echo "$will" | cut -d " " -f1)
-      if [ -z "$(ls modules/ | grep -i -- "$com")" ] || [ -z "$com" ]
-      then
-       echo "TEST SHIT" 
-##./modules/help.sh $who $from >> $input
-        continue
-      fi
-      echo "TEST SHIT"
-## ./modules/$com.sh $who $from $(echo "$will" | cut -d " " -f2-99) >> $input
-    ;;
-    *)
-      echo "$res"
+     str=$(echo "$res" | perl -pe "s/(.*?)\\r/\1/")
+     who=$(echo "$res" | perl -pe "s/:(.*)\!.*@.*/\1/")
+     from=$(echo "$res" | perl -pe "s/.*PRIVMSG (.*[#]?([a-zA-Z]|\-)*) :.*/\1/")
+     msg=$(echo "$str" | perl -pe "s/^.*? PRIVMSG .*? ://")
+twig="<$who> $from :${msg}"
+paste_data "$twig"
     ;;
   esac
 done
